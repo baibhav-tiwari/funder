4e8ccf32f5eebf3b236ee99c9c8a4ce18315fa5c
baibhav tiwari <baibhavtiwari37@gmail.com>
2022-09-20 15:17:19 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
+// api/server.js
+
 const express = require("express")
 const app = express()
+
+app.get("/", function (req, res) {
+    res.send("It's working!")
+})
 
 app.listen(3000, () => {
     console.log("app listening on port 3000")
