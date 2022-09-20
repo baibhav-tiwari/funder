5efd6af3ea7d90c3e634dbf893d0f605d89f8efc
baibhav tiwari <baibhavtiwari37@gmail.com>
2022-09-20 22:02:14 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,3 @@
-// import React, { useEffect, useState } from 'react'
-// function App() {
-//   const [backendData, setBackendData] = useState([{}])
-//   useEffect(() => {
-//     fetch("/api").then(
-//       response => response.json()
-//     ).then(
-//       data => {
-//         setBackendData(data)
-//       }
-//     )
-//   }, [])
-
-
-//   return (
-//     <div>
-//       {(typeof backendData.users === 'undefined') ? (
-//         <p>loading</p>
-//       ) : (
-//         backendData.users.map((user, i) => (
-//           <p key={i}>{user}</p>
-//         ))
-//       )}
-
-//     </div>
-
-//   )
-
-// }
-// export default App
-// import React from 'react';
 import './App.css';
 import {
   BrowserRouter,
